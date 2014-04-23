478c03833b6423bf0302968a80189dcc752eb2ba
Tomasz Panek <joachimsantana@gmail.com>
2014-04-23 13:57:40 +0200

    miniorchanges

--- spec/views/pages/strona_glowna_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-feature 'uzytkownik odwiedza strone glowna' do
-	scenario "powinien widziec logo" do
-		visit root_path
-
-		expect(page).to have_content("Witamy na stronie zrobto")
-	end
-end
